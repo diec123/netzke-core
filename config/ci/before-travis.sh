daca8cf5e71e7b9f3107b51b68c9cedb735a9795
Diego Rodriguez <diec123@yahoo.es>
2012-04-18 19:07:18 +0200

    update to ext-4.0.7

--- config/ci/before-travis.sh
+++ config/ci/before-travis.sh
@@ -2,9 +2,9 @@
 sh -e /etc/init.d/xvfb start
 
 # fetch extjs
-wget http://extjs.cachefly.net/ext-4.0.2a-gpl.zip
-unzip -q -d test/core_test_app/public/ -n ext-4.0.2a-gpl.zip
-mv test/core_test_app/public/ext-4.0.2a test/core_test_app/public/extjs
+wget http://cdn.sencha.io/ext-4.0.7-gpl.zip
+unzip -q -d test/core_test_app/public/ -n ext-4.0.7-gpl.zip
+mv test/core_test_app/public/ext-4.0.7-gpl test/core_test_app/public/extjs
 
 # cp db configuration
 cp test/core_test_app/config/database.yml.travis  test/core_test_app/config/database.yml
