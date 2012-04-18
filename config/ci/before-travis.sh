ddb0716dd47b5b49d0b958618023f04e0365c1f8
Diego Rodriguez <diec123@yahoo.es>
2012-04-18 18:23:24 +0200

    Corrected before-travis.sh

--- config/ci/before-travis.sh
+++ config/ci/before-travis.sh
@@ -3,7 +3,7 @@ sh -e /etc/init.d/xvfb start
 
 # fetch extjs
 wget http://extjs.cachefly.net/ext-4.0.2a-gpl.zip
-unzip -q -d test/basepack_test_app/public/ -n ext-4.0.2a-gpl.zip
+unzip -q -d test/core_test_app/public/ -n ext-4.0.2a-gpl.zip
 mv test/core_test_app/public/ext-4.0.2a test/core_test_app/public/extjs
 
 # cp db configuration
@@ -12,6 +12,7 @@ cp test/core_test_app/config/database.yml.travis  test/core_test_app/config/data
 # create mysql database
 mysql -e 'create database nct_test;'
 
+cd test/core_test_app
 bundle install
 bundle exec rake db:migrate RAILS_ENV=test
 cd ../..
